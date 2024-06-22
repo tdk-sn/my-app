3ce6f230451f4216accbf32059116b59900f9b7b
tdk-sn <89022819+tdk-sn@users.noreply.github.com>
2024-06-22 14:56:22 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formContainer">
+      <form>
+        <h1>ログインフォーム</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>ユーザー名</label>
+            <input type="text" name="username" placeholder="ユーザー名"/>
+          </div>
+          <div className="formField">
+            <label>パスワード</label>
+            <input type="text" name="password" placeholder="パスワード"/>
+          </div>
+          <button className="submitButton">ログイン</button>
+        </div>
+      </form>
     </div>
   );
 }
